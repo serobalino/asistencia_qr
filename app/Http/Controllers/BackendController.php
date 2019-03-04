f628ddc8f4b3c42be56a9f15c57ab814586aeeeb
SebastiaN <odm_sd@hotmail.com>
2019-03-03 20:29:35 -0500

    lista de registrados

---
+++ app/Http/Controllers/BackendController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Pre\Registrados;
+use Illuminate\Http\Request;
+
+class BackendController extends Controller
+{
+    public function __construct(){
+        $this->middleware('auth');
+    }
+
+    public function lista(){
+        return Registrados::with(['discapacidad','tipo','inscripcion','documento'])->orderBy('ape_paterno')->get();
+    }
+}
