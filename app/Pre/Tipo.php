181d369295ba60c5b22a3e03f116315b12b14bff
SebastiaN <odm_sd@hotmail.com>
2019-03-03 20:33:20 -0500

    tipo de inscripcion

---
+++ app/Pre/Tipo.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Pre;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Tipo extends Model
+{
+    protected $connection       =       "mysql2";
+
+    protected $table            =       "tipoinscripcion";
+}
