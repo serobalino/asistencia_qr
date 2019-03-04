e3381a1d40f6187c1b45008c77a5db972c956e57
SebastiaN <odm_sd@hotmail.com>
2019-03-03 20:28:09 -0500

    tabla de asistencia

---
+++ app/Asistencia.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Asistencia extends Model
+{
+    protected $table        =       "asistencias";
+    protected $primaryKey   =       "id_as";
+    public $incrementing    =       false;
+
+}
