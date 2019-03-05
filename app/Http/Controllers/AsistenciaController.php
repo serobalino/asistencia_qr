bac96f2b448e572c47c33e6ab7dae37507fd2a0e
SebastiaN <odm_sd@hotmail.com>
2019-03-05 00:53:13 -0500

    controlador de asistencia

---
+++ app/Http/Controllers/AsistenciaController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+class AsistenciaController extends Controller
+{
+    public function tomar(Request $datos){
+        $validacion =   Validator::make($datos->all(), [
+            'code'  => 'required|string|size:8',
+            'date'  => 'required|date',
+            'time'  => 'required|date',
+        ]);
+        if($validacion->fails()){
+            $texto  =   '';
+            foreach ($validacion->errors()->all() as $errores)
+                $texto.=$errores.PHP_EOL;
+            return (['val'=>false,'mensaje'=>$texto]);
+        }else{
+
+            return (['val'=>true,'mensaje'=>$datos]);
+        }
+    }
+}
