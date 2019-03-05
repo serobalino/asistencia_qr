131bcdb60e7257d1c1ee086f74e9791b0603d504
SebastiaN <odm_sd@hotmail.com>
2019-03-05 00:51:43 -0500

    relacion de horarios

--- app/Curso.php
+++ app/Curso.php
@@ -9,4 +9,8 @@ class Curso extends Model
     protected $table            =       "cursos";
     protected $primaryKey       =       "id_cu";
     public $timestamps          =       false;
+
+    public function horarios(){
+        return $this->hasMany(Horario::class,'id_cu','id_cu');
+    }
 }
