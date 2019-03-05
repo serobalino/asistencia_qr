28a45f3ed41c2779e77c8e996d8131a4d19b5cfc
SebastiaN <odm_sd@hotmail.com>
2019-03-05 01:35:50 -0500

    sin midleware

--- routes/api.php
+++ routes/api.php
@@ -17,4 +17,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::post('/tomar', "AsistenciaController@tomar")->middleware('auth:api');
+Route::post('/tomar', "AsistenciaController@tomar");
