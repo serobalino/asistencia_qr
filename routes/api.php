675e473eb4c3fb9680ba5b361ee794d11d4cd194
SebastiaN <odm_sd@hotmail.com>
2019-03-05 01:00:02 -0500

    tomar asistencia por api

--- routes/api.php
+++ routes/api.php
@@ -17,4 +17,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('qr/{token}','QrController@crear')->name('generar');
+Route::post('/tomar', "AsistenciaController@tomar")->middleware('auth:api');
