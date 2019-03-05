a67ed0eee71c98bce6687f2a9ea4c81ab9e78c1b
SebastiaN <odm_sd@hotmail.com>
2019-03-05 00:57:59 -0500

    historial de usuario asistencias tomadas

---
+++ resources/js/components/historia.vue
@@ -0,0 +1,60 @@
+<template>
+    <div class="col-md-10">
+        <div class="card">
+            <div class="card-body animated fadeIn">
+                <vue-good-table
+                        styleClass="table table-hover"
+                        :columns="columns"
+                        :rows="rows"
+                />
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import { VueGoodTable } from 'vue-good-table';
+    export default {
+        name: "historia",
+        components: {
+            VueGoodTable,
+        },
+        data:()=>({
+            columns: [
+                {
+                    label: 'Detalle',
+                    field: 'detalle_ca',
+                    filterOptions: {
+                        enabled: true,
+                        placeholder: 'Filtro por siglas',
+                    },
+                },
+                {
+                    label: 'Estado',
+                    field: 'estado_ca',
+                },
+                {
+                    label: 'Empieza',
+                    field: 'desde_ca',
+                    filterOptions: {
+                        enabled: true,
+                        placeholder: 'Filtro por fecha',
+                    },
+                },
+                {
+                    label: 'Finaliza',
+                    field: 'vence_ca',
+                    filterOptions: {
+                        enabled: true,
+                        placeholder: 'Filtro por fecha',
+                    },
+                },
+            ],
+            rows: [],
+        }),
+    }
+</script>
+
+<style scoped>
+
+</style>
