ecaddbed86075f4c1b386a272f140f61e618b3c4
SebastiaN <odm_sd@hotmail.com>
2019-03-05 00:53:23 -0500

    uso de usuario

--- app/Http/Controllers/BackendController.php
+++ app/Http/Controllers/BackendController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Pre\Registrados;
+use App\User;
 use Illuminate\Http\Request;
 
 class BackendController extends Controller
@@ -14,4 +15,5 @@ class BackendController extends Controller
     public function lista(){
         return Registrados::with(['discapacidad','tipo','inscripcion','documento'])->orderBy('ape_paterno')->get();
     }
+
 }
