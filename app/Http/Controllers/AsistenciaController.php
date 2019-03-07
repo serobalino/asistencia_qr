cbd10a4a920816251c45eda7470701ccd8fd3d51
SebastiaN <odm_sd@hotmail.com>
2019-03-06 19:58:53 -0500

    correccion de hash

--- app/Http/Controllers/AsistenciaController.php
+++ app/Http/Controllers/AsistenciaController.php
@@ -32,9 +32,9 @@ class AsistenciaController extends Controller
 
             $texto      =   $datos->type ? "Registered checkin" : "Registered checkout";
             if($registro && $curso){
-                /*
+
                 $inscripcion            =   new Asistencia();
-                $inscripcion->id_as     =   md5($registro->id.now());
+                $inscripcion->id_as     =   md5($registro->id.$datos->type."$datos->date $datos->time");
                 $inscripcion->id_es     =   $registro->id;
                 $inscripcion->id_cu     =   $curso->id_cu;
                 $inscripcion->tipo_as   =   $datos->type;
@@ -46,7 +46,7 @@ class AsistenciaController extends Controller
                 $log->id_es     =   $registro->id;
                 $log->id_as     =   $inscripcion->id_as;
                 $log->detalle_lo=   $texto;
-                $log->save();*/
+                $log->save();
 
                 return (['val'=>true,'mensaje'=>"$registro->name $texto ,successfully"]);
             }else{
