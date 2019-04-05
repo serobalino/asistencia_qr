60dd232fbaed5b8500c8443a5ddaaa0ee235f6c5
SebastiaN <odm_sd@hotmail.com>
2019-04-04 23:44:01 -0500

    fecha serializada

--- app/Asistencia.php
+++ app/Asistencia.php
@@ -2,7 +2,9 @@
 
 namespace App;
 
+use App\Pre\Registrados;
 use Illuminate\Database\Eloquent\Model;
+use Jenssegers\Date\Date;
 
 class Asistencia extends Model
 {
@@ -10,11 +12,18 @@ class Asistencia extends Model
     protected $primaryKey   =       "id_as";
     public $incrementing    =       false;
 
-    protected $appends = ['tipo'];
+    protected $appends = ['tipo','creado'];
 
     protected $connection   =   "mysql";
 
     public function getTipoAttribute(){
         return $this->attributes['tipo_as'] ? 'Checkin' : 'Checkout';
+    }
+    public function getCreadoAttribute(){
+        return Date::createFromFormat('Y-m-d H:i:s',$this->attributes['created_at'])->diffForHumans();
+    }
+
+    public function dueno(){
+        return $this->hasOne(Registrados::class,"id","id_es");
     }
 }
